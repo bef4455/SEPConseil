182b81489b70a9ba3ccfef20d40b51cd7a604589
bef4 <befacool@gmail.com>
2024-05-15 19:45:56 +0200

    fix '

--- app/components/Aboutus/index.tsx
+++ app/components/Aboutus/index.tsx
@@ -43,7 +43,7 @@ const Aboutus = () => {
             <div className='mx-auto max-w-7xl px-4 py-24 my-32 lg:px-10 bg-lightgrey rounded-3xl relative'>
                 <Image src="/images/aboutus/dots.svg" width={100} height={100} alt="dots-image" className="absolute bottom-1 -left-20" />
                 <h3 className='text-center text-blue text-lg tracking-widest'>Nos Services</h3>
-                <h4 className='text-center text-4xl lg:text-65xl font-bold'>Services de gestion d'entreprise</h4>
+                <h4 className='text-center text-4xl lg:text-65xl font-bold'>Services de gestion d&apos;entreprise</h4>
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-16 gap-x-16 lg:gap-x-32'>
                     {Aboutdata.map((item, index) => (
                         <div key={index} className='hover:bg-navyblue bg-white rounded-3xl mt-16 pt-10 pl-8 pb-10 pr-6 shadow-xl group transform transition duration-500 hover:scale-105'>
